de4c4f9a122be45c1ccd0a0fb46b8f3007682799
= <intanmeilanienugraha@gmail.com>
2023-03-05 22:04:30 +0700

    UPDATE

--- script.js
+++ script.js
@@ -116,7 +116,10 @@
 // kota[1]; // memanggil index array
 // kota[2].namaKota; //memanggil object dalam index array
 
-//------PERULANGAN FOR------
+//------PERULANGAN/LOOPING------
+
+//---FOR---
+
 // for (let i = 1; i <= 10; i++) {
 //   console.log(`ini adalah angka ke-${i}`);
 // }
@@ -132,3 +135,110 @@
 // for (let i = 20; i <= 200000; i *= 10) {
 //   console.log(`ini adalah perkalian ke-${i}`); //contoh 4
 // }
+
+//---LOOPING ARRAY (FOR)---
+// const buah = ["apel", "anggur", "jeruk", "salak", "nanas"];
+
+// for (let i = 0; i < buah.length; i++) {
+//   console.log(i, buah[i]);
+// }
+
+//------NESTED LOOP------
+//Example 1
+// const pilihan = "abcde";
+// for (let i = 1; i <= 5; i++) {
+//   console.log(`${i}. Soal Nomor ${i}`);
+//   for (let j = 0; j < pilihan.length; j++) {
+//     console.log(`   ${pilihan[j]}. Pilihan Jawaban`);
+//   }
+// }
+
+//Example 2 Nested Array with Nested Loop
+// const studenRow = [
+//   ["Dewo", "Wahyu", "Mustika", "Wiwaha"],
+//   ["intan", "Meilanie", "Nugraha", "Nazla"],
+//   ["Jaliah", "Kokom", "Indah", "Abdul"],
+// ];
+
+// for (let i = 0; i < studenRow.length; i++) {
+//   const row = studenRow[i];
+//   console.log(`Seat Row #${i + 1}`);
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(`   ${row[j]}`);
+//   }
+// }
+
+//---WHILE---
+//Example 1
+// const pw = "pass200502";
+// let guess = prompt("enter password");
+// while (guess !== pw) {
+//   guess = prompt("enter password");
+// }
+// alert("Password is correct");
+
+//Example 2
+// let input = prompt("say something!");
+// while (true) {
+//   input = prompt(input);
+//   if (input.toLowerCase === "stop") break;
+// }
+// alert("OK");
+
+//---GAME WITH WHILE---
+// let maximum = parseInt(prompt('Masukkan Nilai Maksimal');
+// while (maximum) {
+//     maximum = parseInt(prompt('Masukkan Nilai Maksimal');
+// }
+// const targetNumber = math.floor(math.random() * maximum) + 1;
+// console.log(targetNumber);
+
+// let guess = parseInt(prompt('Isi tebakan pertama');
+// let attempt = 1;
+
+// while (parseInt(guess) !== targetNumber) {
+//     attempt++;
+//     if (guess > targetNumber) {
+//         guess = parseInt(prompt('Terlalu Tinggi! Tebak Lagi:'));
+//     } else {
+//         guess = parseInt(prompt('Terlalu Rendah! Tebak Lagi:'));
+//     }
+// }
+// alert(`Selamat Tebakkan Anda Benar! Dengan Percobann ${attempt} Kali`);
+
+//---FOR OF---
+//Example 1
+// const fruits = ["Apel", "Mangga", "Jeruk", "Anggur"];
+// for (let fruit of fruits) {
+//   console.log(`Buah ${fruit}`);
+// }
+
+//Example 2 With Nested Array
+// const studenRow = [
+//   ["Dewo", "Wahyu", "Mustika", "Wiwaha"],
+//   ["intan", "Meilanie", "Nugraha", "Nazla"],
+//   ["Jaliah", "Kokom", "Indah", "Abdul"],
+// ];
+// for (let row of studenRow) {
+//   for (let student of row) {
+//     console.log(student);
+//   }
+// }
+
+//---FOR IN---
+// const score = {
+//   Intan: 10,
+//   In: 20,
+//   tan: 10,
+// };
+// for (let student in score) {
+//   console.log(`${student} Memiliki Skor ${score[student]}`);
+// }
+
+//------FUNCTION------
+// function wowo() {
+//   console.log("Bau Banget");
+//   console.log("BOTAKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
+//   console.log("HAHAHAHA WWKWKWKKWKWKWKWKWKWKWKWKW");
+// }
+// wowo();
